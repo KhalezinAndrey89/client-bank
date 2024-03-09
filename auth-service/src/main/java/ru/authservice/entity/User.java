02f5efb7f9ee0b072ee1139b061c2a0ac47c44b6
Khalezin Andrey <66243527+KhalezinAndrey89@users.noreply.github.com>
2024-03-09 17:10:51 +0300

    Task5: add entity User in auth service

---
+++ auth-service/src/main/java/ru/authservice/entity/User.java
@@ -0,0 +1,27 @@
+package ru.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+}
